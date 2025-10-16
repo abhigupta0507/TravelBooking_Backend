408430f3af3baaecd4888da121f45aa43381d81e
Arshixs <arshnatani123456@gmail.com>
2025-10-16 17:29:58 +0530

    Changes in DTO and Schema

--- src/main/java/com/example/demo/model/IncludeRooms.java
+++ src/main/java/com/example/demo/model/IncludeRooms.java
@@ -4,13 +4,18 @@ public class IncludeRooms {
     private Integer package_id;
     private Integer hotel_id;
     private Integer room_id;
+    private Integer check_in_day;
+    private Integer check_out_day;
+
 
     public IncludeRooms() {}
 
-    public IncludeRooms(Integer package_id, Integer hotel_id, Integer room_id) {
+    public IncludeRooms(Integer package_id, Integer hotel_id, Integer room_id, Integer check_in_day, Integer check_out_day) {
         this.package_id = package_id;
         this.hotel_id = hotel_id;
         this.room_id = room_id;
+        this.check_in_day = check_in_day;
+        this.check_out_day = check_out_day;
     }
 
     public Integer getPackage_id() {
@@ -37,12 +42,21 @@ public class IncludeRooms {
         this.room_id = room_id;
     }
 
+    public Integer getCheck_out_day() { return check_out_day; }
+    public void setCheck_out_day(Integer check_out_day) { this.check_out_day = check_out_day; }
+
+    public Integer getCheck_in_day() { return check_in_day; }
+    public void setCheck_in_day(Integer check_in_day) { this.check_in_day = check_in_day; }
+
+
     @Override
     public String toString() {
-        return "Include_Rooms{" +
+        return "IncludeRooms{" +
                 "package_id=" + package_id +
                 ", hotel_id=" + hotel_id +
                 ", room_id=" + room_id +
+                ", check_in_day=" + check_in_day +
+                ", check_out_day=" + check_out_day +
                 '}';
     }
 }
